5ad01f914808c12a951a4ac30b7911ecd008e395
Manish Bhatt <mbhatt@uno.edu>
2019-02-08 15:59:42 -0600

    Dockerinterface don\'t need 02/08/2019

--- jobs/StateManager/config.py
+++
@@ -1,3 +0,0 @@
-# from celery import Celery
-
-# app = Celery('celery_config', broker='redis://localhost:6379/0', include=['celery_blog'])
--- lib/DockerInterface.py
+++ lib/DockerInterface.py
@@ -1,9 +1,36 @@
-import docker
+# import docker
 
 
-'''
-1. This api should be able to create n-nugget containers for apocaplyse
-2. This api should be able to put evidence in a volume, and attach the said volume to containers
-3. This api should be able to keep track of the containers it created
-3.1 The keeping track could be done at a higher level as well
-'''
+# '''
+# 1. This api should be able to create n-nugget containers for apocaplyse
+# 2. This api should be able to put evidence in a volume, and attach the said volume to containers
+# 3. This api should be able to keep track of the containers it created
+# 3.1 The keeping track could be done at a higher level as well
+# '''
+
+# def get_client():
+#     return docker.from_env()
+
+
+# def get_events(client):
+#     events = client.events()
+#     return events
+
+
+# def build_image(client, filepath='', imagetag=''):
+#     client.images.build(path=filepath, tag=imagetag)
+#     return
+
+# def create_container(client, imagetag='', containername=''):
+#     client.containers.run("ubuntu:latest", "/bin/bash", detach=True, stdin_open=True, stdout=True, tty=True)
+#     return
+    
+
+# def create_n_containers(client, number=1, imagename='', containergroupname=''):
+#     a = []
+#     for i in range(0,number):
+#         containername = containergroupname+str(i)
+#         a.append(create_container(client, imagetag=imagename, containername=containername))
+#     return a
+
+
